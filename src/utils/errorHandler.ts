1e055f0ea035f92efbbe6f5a5a548c1a68a42492
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-05-25 00:09:11 +0530

    update errorHandler

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,37 +1,5 @@
-// Import necessary node Module(s)
-import fs from "node:fs";
-
 // Import necessary Module(s)
 import { lightRed } from "kolorist";
-
-// Import necessary Type(s)
-import type { T_UserInput } from "../types/prompt";
-
-const cwd = process.cwd();
-let targetPath: string;
-export function makeTargetPath(userInput: T_UserInput) {
-    targetPath = `${cwd}/${userInput.dirName}`;
-
-    return targetPath;
-}
-
-export async function rollBack() {
-    fs.rmdir(targetPath, (err) => {
-        if (err) {
-            console.log(err);
-
-            console.log(
-                lightRed(
-                    "An Error has occured while rolling back...\n We recommend you to manually delete the project directory.",
-                ),
-            );
-
-            process.exit(1);
-        }
-    });
-
-    process.exit(1);
-}
 
 // biome-ignore lint/suspicious/noExplicitAny: <errors can be of any type>
 function handleError(err: any) {
@@ -39,11 +7,11 @@ function handleError(err: any) {
 
     console.log(
         lightRed(
-            "An Error has occured while setting up your project...\nWe have undone any changes the script made",
+            "An Error has occured while setting up your project...\nWe recommend you to manually delete the project directory.",
         ),
     );
 
-    rollBack();
+    process.exit(1);
 }
 
 export default handleError;
